b291a5a6c6f528c9bb0bdc38589ef442f68514e8
yeolsss <yeol8810@gmail.com>
2024-05-19 16:59:03 +0900

    refactor: - modal component를 import할 때 lazy 적용

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,8 +1,10 @@
 import Page from "@/components/page";
 import Button from "@/components/button";
 import { useModalContext } from "@/context/modalContext";
-import LoginModal from "@/components/loginModal";
-import SignUpModal from "@/components/signUpModal";
+import { lazy, Suspense } from "react";
+
+const LoginModal = lazy(() => import("@/components/loginModal"));
+const SignUpModal = lazy(() => import("@/components/signUpModal"));
 
 function HomePage() {
   const modal = useModalContext();
@@ -14,10 +16,27 @@ function HomePage() {
       </h1>
 
       <div className="flex flex-col gap-10 mx-auto">
-        <Button onClick={() => modal.open(<LoginModal />, false)}>
+        <Button
+          onClick={() =>
+            modal.open(
+              <Suspense fallback={null}>
+                <LoginModal />
+              </Suspense>,
+              false,
+            )
+          }
+        >
           Login Modal
         </Button>
-        <Button onClick={() => modal.open(<SignUpModal />)}>
+        <Button
+          onClick={() =>
+            modal.open(
+              <Suspense fallback={null}>
+                <SignUpModal />
+              </Suspense>,
+            )
+          }
+        >
           Signup Modal
         </Button>
       </div>
